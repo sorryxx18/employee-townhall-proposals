d28eeefd7b4e68e2c6b8ffb68f98c9336c408e39
sorryxx18 <157127756+sorryxx18@users.noreply.github.com>
2026-03-12 13:08:39 +0800

    Config: point GAS_URL to working deployment

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@
 // 注意：這個檔案會被 index.html 直接載入，請避免放敏感資訊。
 window.APP_CONFIG = {
   // ⚠️ 務必替換此網址為您的 GAS 部署網址
-  GAS_URL: "https://script.google.com/macros/s/AKfycbwihhmMn0cYgxmdlRBM2L-FZtvjIEh2o4BvpY21Pq30B2rZO3PRZaK9wz8-ZN-na6Ur/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxP19c3OaDUo4diW9LAPQyDyLRud-ICNa_nLEs2smtu0P63OIrC50ibnxQGyjrMn-xw/exec",
   // API_TOKEN：請填入 Apps Script 的 Script Properties（API_TOKEN）同一串
   API_TOKEN: "tfd-townhall-2026-03-12_9Jc4pR2kV7mL1sN8aZ0wY6uH4cD5eTQx"
 };
