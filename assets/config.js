4e68ada140b85861c6655c72b5a46d73a8d1f32a
Leif Huang <leifhuang@LeifdeMac-mini.local>
2026-03-12 21:20:26 +0800

    Config: update GAS_URL to new deployment with HTML response

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@
 // 注意：這個檔案會被 index.html 直接載入，請避免放敏感資訊。
 window.APP_CONFIG = {
   // ⚠️ 務必替換此網址為您的 GAS 部署網址
-  GAS_URL: "https://script.google.com/macros/s/AKfycbzb11cyVJ6zRStXFw05I6upDoyo8TRX0_Y7dIsgjy7z40rRnCCuEd0-Bqsde7kQpRqs/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbynjZTt5-2kVdj96PjZGCQVL0RxwQcp6pJ4xDSoEbITlBDj-ptYBCMgixX_bx83NHnH/exec",
   // API_TOKEN：請填入 Apps Script 的 Script Properties（API_TOKEN）同一串
   API_TOKEN: "tfd-townhall-2026-03-12_9Jc4pR2kV7mL1sN8aZ0wY6uH4cD5eTQx"
 };
