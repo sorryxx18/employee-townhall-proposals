6c4205714738dc5e1eb0c6da49eb163a037b7125
sorryxx18 <157127756+sorryxx18@users.noreply.github.com>
2026-03-12 11:58:02 +0800

    Config: update GAS_URL deployment endpoint

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@
 // 注意：這個檔案會被 index.html 直接載入，請避免放敏感資訊。
 window.APP_CONFIG = {
   // ⚠️ 務必替換此網址為您的 GAS 部署網址
-  GAS_URL: "https://script.google.com/macros/s/AKfycbw1xL09BkHPxdFE8YVi7BJ9wukbqLbHLdx3bEVFg1VUndWm_x_Dc591fKI4h0EdSm8i/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwihhmMn0cYgxmdlRBM2L-FZtvjIEh2o4BvpY21Pq30B2rZO3PRZaK9wz8-ZN-na6Ur/exec",
   // API_TOKEN：請填入 Apps Script 的 Script Properties（API_TOKEN）同一串
   API_TOKEN: ""
 };
