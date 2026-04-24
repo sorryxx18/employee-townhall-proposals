0cd22e7344d8a034d2ff616a5e183dcacb505b22
Leif Huang <leifhuang@LeifdeMac-mini.local>
2026-04-24 22:54:42 +0800

    chore: point townhall frontend to latest GAS deployment

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@
 // 注意：這個檔案會被 index.html 直接載入，請避免放敏感資訊。
 window.APP_CONFIG = {
   // ⚠️ 務必替換此網址為您的 GAS 部署網址
-  GAS_URL: "https://script.google.com/macros/s/AKfycby_dPg5ZfHAHrIjWhbjQiEkP9yipG0L6OLGLPDz_egGdEtE4znn7fDkfcMCuJ3-IbXT/exec", 
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxjm29FYt8z3zrR2sclwCkK4ph3GWSUBuDKuT8tydER8xy-nzcjk48q1ZHDSwGGhBks/exec", 
   // API_TOKEN：請填入 Apps Script 的 Script Properties（API_TOKEN）同一串
   API_TOKEN: "tfd-townhall-2026-03-12_9Jc4pR2kV7mL1sN8aZ0wY6uH4cD5eTQx"
 };
